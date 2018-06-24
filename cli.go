a553b942e5b8cba32c7b8fb8087b6dc86e6412f0
shiimaxx <yoshima@shiimaxx.com>
2018-06-25 08:47:03 +0900

    Flags

--- cli.go
+++ cli.go
@@ -5,6 +5,10 @@ import (
 
 	"fmt"
 
+	"flag"
+
+	"os"
+
 	"github.com/shiimaxx/alfred-gitlab-workflow/workflow"
 )
 
@@ -23,6 +27,40 @@ type CLI struct {
 
 // Run invokes the CLI with the given arguments.
 func (c *CLI) Run(args []string) int {
+	var (
+		setURL   bool
+		setToken bool
+	)
+	flags := flag.NewFlagSet("alfred-gitlab-workflow", flag.ContinueOnError)
+	flags.SetOutput(c.outStream)
+	flags.BoolVar(&setURL, "set-url", false, "set endpoint url")
+	flags.BoolVar(&setToken, "set-token", false, "set personal access token")
+	if err := flags.Parse(args[1:]); err != nil {
+		return ExitCodeError
+	}
+
+	if setURL {
+		f, err := os.OpenFile("endpoint_url", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0664)
+		if err != nil {
+			fmt.Fprint(c.errStream, err)
+			return ExitCodeError
+		}
+		url := flags.Args()[0]
+		if _, err := f.Write([]byte(url)); err != nil {
+			fmt.Fprint(c.errStream, err)
+			return ExitCodeError
+		}
+		if err := f.Close(); err != nil {
+			fmt.Fprint(c.errStream, err)
+			return ExitCodeError
+		}
+		return ExitCodeOK
+	}
+
+	if setToken {
+		return ExitCodeOK
+	}
+
 	fmt.Fprint(c.outStream, workflow.Run())
 	return ExitCodeOK
 }
